f3a38a2659de7fc5b76e338ea772a20aa7adaa81
Sam Guard <sam.guard@outlook.com>
2022-04-20 12:21:39 +0100

    ordered them correctly

--- task2.py
+++ task2.py
@@ -1,9 +1,10 @@
+from cgi import test
 from doctest import testfile
 from fileinput import filename
 from multiprocessing.dummy import Array
 from numbers import Number
 from tokenize import String
-from cv2 import imread
+from cv2 import imread, sort
 import numpy as np
 import cv2 as cv
 import math
@@ -20,47 +21,58 @@ def main():
     pass
 
 
-def loadImgData(dir: str) -> List[Any]:
+def loadTrainData(dir: str) -> List[Any]:
     return list(map(lambda fileName: [fileName[4:], cv.imread(dir + fileName)],  os.listdir(dir)))
 
 
+def loadTestData(dir: str) -> List[Any]:
+    return sorted(
+        list(
+            map(lambda fileName: [int(fileName.split("_")[2].split(".")[0]), cv.imread(dir + fileName)],
+                os.listdir(dir))),
+            key=lambda x: x[0])
+
+
 def loadAnnotations(dir: str):
-    textFiles: List[List[str]] = list(
+    textFiles: List[List[int, List[str]]] = list(
         # filter all indexs that are -1
-        filter(lambda x: x != -1,
+        filter(lambda x: x[1] != -1,
                # Read in all files in dir that end in .txt otherwise return -1
-               map(lambda fileName: -1 if
+               map(lambda fileName: [-1, -1] if
                    (fileName[-4:] != ".txt")
                    else
-                   open(dir + fileName, "r").readlines(), os.listdir(dir)
+                   [int(fileName.split("_")[2].split(".")[0]),
+                    open(dir + fileName, "r").readlines()],
+                   os.listdir(dir)
                    )
                )
     )
 
-    out: List[List[str, float, float, float, float]] = []
+    out: List[List[int, List[str, float, float, float, float]]] = []
     for data in textFiles:
-      out.append([])
-      #For each line in a file
-      for l in data:
-        if(len(l) == 0):
-          continue
-        parts = l.split(", ")
-        iconName: str = parts[0]
-        # Points for the icon
-        px1: float = float(parts[1][1:])
-        py1: float = float(parts[2][:-1])
-        px2: float = float(parts[3][1:])
-        py2: float = float(parts[4][:-2])
-        out[-1].append([iconName, px1, py1, px2, py2])
-    return out
+        out.append([data[0]])
+        # For each line in a file
+        for l in data[1]:
+            if(len(l) == 0):
+                continue
+            parts = l.split(", ")
+            iconName: str = parts[0]
+            # Points for the icon
+            px1: float = float(parts[1][1:])
+            py1: float = float(parts[2][:-1])
+            px2: float = float(parts[3][1:])
+            py2: float = float(parts[4][:-2])
+            out[-1].append([iconName, px1, py1, px2, py2])
 
+    return sorted(out, key=lambda x: x[0])
 
 
 print("Loading data...")
-trainData: List[List] = loadImgData(TRAIN_DATA_DIR)
-testData: List[List] = loadImgData(TEST_DATA_DIR)
+trainData: List[List] = loadTrainData(TRAIN_DATA_DIR)
+testData: List[List] = loadTestData(TEST_DATA_DIR)
 annotData: List[Any] = loadAnnotations(ANNOTATION_DATA_DIR)
 
 print("Done")
 print("Train data size:", len(trainData))
 print("Test data size:", len(testData))
+
