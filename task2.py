cd2e0b699915eb2d7c8fb6b95aa192680348e1b9
Sam Guard <sam.guard@outlook.com>
2022-04-21 13:38:18 +0100

    yasss task 2 going well

--- task2.py
+++ task2.py
@@ -11,21 +11,23 @@ import math
 from matplotlib import pyplot as plt
 from typing import Any, List, Set, Dict, Tuple, Optional
 import os
+from scipy.ndimage import gaussian_filter
 
 TRAIN_DATA_DIR = "task2/Training/png/"
 TEST_DATA_DIR = "task2/TestWithoutRotations/images/"
 ANNOTATION_DATA_DIR = "task2/TestWithoutRotations/annotations/"
 
+
 def loadTrainData(dir: str) -> List[Any]:
-    return list(map(lambda fileName: [fileName[4:], cv.imread(dir + fileName)],  os.listdir(dir)))
+    return list(map(lambda fileName: [fileName[4:], cv.imread(dir + fileName, cv.COLOR_BGR2GRAY)],  os.listdir(dir)))
 
 
 def loadTestData(dir: str) -> List[Any]:
     return sorted(
         list(
-            map(lambda fileName: [int(fileName.split("_")[2].split(".")[0]), cv.imread(dir + fileName)],
+            map(lambda fileName: [int(fileName.split("_")[2].split(".")[0]), cv.imread(dir + fileName, cv.COLOR_BGR2GRAY)],
                 os.listdir(dir))),
-            key=lambda x: x[0])
+        key=lambda x: x[0])
 
 
 def loadAnnotations(dir: str):
@@ -62,18 +64,71 @@ def loadAnnotations(dir: str):
     return sorted(out, key=lambda x: x[0])
 
 
-def main():  
-  print("Loading data...")
-  trainData: List[List] = loadTrainData(TRAIN_DATA_DIR)
-  testData: List[List] = loadTestData(TEST_DATA_DIR)
-  annotData: List[Any] = loadAnnotations(ANNOTATION_DATA_DIR)
+# Given an array of scaled icon images find the best match in the image
+def getBestMatchForIcon(img, pyramid):
+    bestScale = 1
+    bestScore = 1
+    bestRes = None
+    bestPos = None
+    for i in range(len(pyramid) - 1, -1, -1):
+        res = cv.matchTemplate(img, pyramid[i], cv.TM_SQDIFF_NORMED)
+        minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(res)
+        closer = min(minVal, maxVal)
+        if(closer < bestScore):
+            bestScale = i
+            bestScore = closer
+            bestRes = res
+            bestPos = minLoc if (minVal < maxVal) else maxLoc
 
-  print("Done")
-  print("Train data size:", len(trainData))
-  print("Test data size:", len(testData))
+    '''
+    plt.subplot(111), plt.imshow(bestRes)
+    plt.scatter(bestPos[0], bestPos[1])
+    plt.show()
+    print("best scale", bestScale)
+    '''
+
+    return [bestScore, bestScale, bestPos]
+
+
+def main():
+    print("Loading data...")
+    iconData: List[List] = loadTrainData(TRAIN_DATA_DIR)
+    testData: List[List] = loadTestData(TEST_DATA_DIR)
+    annotData: List[Any] = loadAnnotations(ANNOTATION_DATA_DIR)
+
+    print("Done")
+    print("Train data size:", len(iconData))
+    print("Test data size:", len(testData))
+
+    image = testData[0][1]
+
+    '''
+    plt.subplot(131), plt.imshow(image, cmap="Greys")
+    plt.title('Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(132), plt.imshow(icon, cmap="Greys")
+    plt.title('Icon'), plt.xticks([]), plt.yticks([])
+    plt.show()
+    '''
+
+    iconScores = []
+    num = 0
+    for _, icon in iconData:
+        iconPyramid = [icon]
+
+        for i in range(4):
+            iconPyramid.append(cv.pyrDown(iconPyramid[-1]))
+            #plt.subplot(141 + i), plt.imshow(imgPyramid[-1], cmap="Greys")
+            #plt.subplot(141 + i), plt.imshow(iconPyramid[-1], cmap="Greys")
+
+        iconScores.append([num] + getBestMatchForIcon(image, iconPyramid))
+        num += 1
+
+    iconScores = sorted(iconScores, key=lambda x: x[1])
+    for x in iconScores:
+        print(x)
+
+    for i in range(4):
+        print(iconData[iconScores[i][0]][0])
 
 
 main()
-
-
-
