c0d833659716c97f5bdcbaaaedce51b691b3937f
Tobias Patten <tobypatten@gmail.com>
2022-04-28 18:33:19 +0100

    Update task2IntMatch.py
    change IntMatch to start on templateMatching

--- task2IntMatch.py
+++ task2IntMatch.py
@@ -1,106 +1,34 @@
 import math
+import signal
 
 import cv2 as cv
-import imutils as imutils
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.ndimage import gaussian_filter
 
-image = cv.imread('data2tests/test_image_1.png')
-imageGray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-print(image)
-template = cv.imread('data2temp/011-trash.png')
-template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
-print(template)
-w, h = template.shape[::-1]
-_,imageSize = imageGray.shape
-print(template.shape)
-print(imageGray.shape)
-_,size = template.shape
+def matchTemplateHome(image, template):
+    image = image[0:,0:,1]
+    template = template[0:,0:,1]
 
-
-
-
-x=0
-y=0
-
-# GRAY IMAGE
-imageAverage = 0
-imageSD = 0
-
-for x in range(imageSize):
-    for y in range(imageSize):
-        imageAverage = imageAverage + imageGray[x][y]
-
-imageAverage = imageAverage / (imageSize * imageSize)
-
-for x in range(imageSize):
-    for y in range(imageSize):
-        imageSD = imageSD + (imageGray[x][y] - imageAverage) ** 2
-
-imageSD = math.sqrt(imageSD)
-
-
-imageNormalised = [[0 for x in range(imageSize)] for y in range(imageSize)]
-
-for x in range(size):
-    for y in range(size):
-        imageNormalised[x][y] = (imageGray[x][y] - imageAverage) / imageSD
-
-imageNormalised = np.array(imageNormalised)
-
-template = gaussian_filter(template, sigma=1)
-template = template[0::2,0::2]
-print(template.shape)
-_, size = template.shape
-
-
-while(size>31):
-
-    template = gaussian_filter(template, sigma=1)
-    template = template[0::2,0::2]
-
-    print(template.shape)
     _, size = template.shape
+    _, imageSize = image.shape
 
-    templateNormalised = [[0 for x in range(size)] for y in range(size)]
+    #TEMPLATE Norm
+    templateAverage = np.mean(template)
+    templateSD = np.std(template)
 
-    #TEMPLATE
-    templateAverage = 0
-    templateSD = 0
-    for x in range(size):
-        for y in range(size):
-            templateAverage = templateAverage + template[x][y]
+    templateNormalised = [[(template[x][y] - templateAverage) / templateSD for x in range(size)] for y in range(size)]
 
-    templateAverage = templateAverage/(size*size)
+    #IMAGE Norm
+    imageAverage = np.mean(image)
+    imageSD = np.std(image)
 
-    for x in range(size):
-        for y in range(size):
-            templateSD = templateSD + (template[x][y] - templateAverage)**2
+    imageNormalised = [[(image[x][y] - imageAverage) / imageSD for x in range(imageSize)] for y in range(imageSize)]
 
-    templateSD = math.sqrt(templateSD)
+    #Convolve???? I have tried multiple ways of achieving f hat * g hat from lectures with no sucsess
+    heatmap = signal.convolve2d(imageNormalised,templateNormalised, mode= "valid")
 
-    for x in range(size):
-        for y in range(size):
-            templateNormalised[x][y] = (template[x][y]-templateAverage)/templateSD
+    return (heatmap)
+    #return (heatmap / (size ** 2))
 
-    heatmap = [[0 for x in range(imageSize - size +1)] for y in range(imageSize - size +1)]
-
-    for x in range(imageSize - size +1):
-        for y in range(imageSize - size + 1):
-            total = 0
-
-            heatmap[x][y] = (np.dot(np.array(imageNormalised[x:x+size, y:y+size]).flatten(),np.array(templateNormalised).flatten()) +1)*128
-            #print(heatmap[x][y])
-            #for x2 in range(size):
-            #    for y2 in range(size):
-            #        total = total + (templateNormalised[x2][y2] * imageNormalised[x+x2][y+y2])
-            #heatmap[x][y] = total
-        print("done" + str(x))
-    print(np.max(heatmap))
-
-
-    plt.imshow(heatmap, cmap='gray', vmin=0, vmax=255)
-    plt.show()
 
 
